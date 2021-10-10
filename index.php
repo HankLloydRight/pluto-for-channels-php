0b53b745c27d7f3bd26d8db37d2745fd05bf48ba
Hank Lloyd Right <hanklloydright@gmail.com>
2021-10-10 14:19:22 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<style>
+body {background:black;color:white;}
+a {color:green;}
+</style>
+
+<h2>
+	These files are updated every 3 hours.
+	Unique deviceID and sid parameters are inserted into the stream URLs every time the playlist link below file is called.<Br>
+	This should prevent stream conflicts and dropouts.<Br>
+<br>
+	Thank you, Channels Devs for the <a href=https://github.com/maddox/pluto-for-channels>maddox/pluto-for-channels</a> repo which this page calls in the background.
+	<hr>
+<br>
+To use these links, just right-click and "Copy Link Address", then paste into the Channels Admin page for Pluto settings.	<br>
+<a href=pluto/playlist.m3u>Pluto Playlist.m3u</a>
+<br>
+<a href=pluto/epg.xml>Pluto epg.xml</a>
+<br><br>Files last updated:
+<?
+$f=filemtime("pluto/playlist.m3u");
+		date_default_timezone_set('America/New_York');
+		echo  date(DATE_RFC2822,$f);//"l F jS   Y h:i:s A T"
+	?>
+<hr>
+This project on Github: <a href=https://github.com/HankLloydRight/pluto-for-channels-php>pluto-for-channels-php</a>
+
