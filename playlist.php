5f83ecc32fb669162c08845a9cce31855bc3aa22
Hank Lloyd Right <56199831+HankLloydRight@users.noreply.github.com>
2021-10-12 10:52:17 -0400

    Removed the need to update the SID parameter
    Only deviceID needs to be made unique

--- playlist.php
+++ playlist.php
@@ -14,15 +14,6 @@ header('Content-Disposition: attachment; filename=playlist.m3u');
 // generates a V1 UUID
 include("uuid.inc");
 
-// generates a V4 uuid
-function guidv4() {
-	$data=openssl_random_pseudo_bytes(16);
-    assert(strlen($data) == 16);
-    $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // set version to 0100
-    $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // set bits 6-7 to 10
-    return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-}
-
 //generate a mac address for the entire run
 $mac=implode(':', str_split(substr(md5(mt_rand()), 0, 12), 2));
 
@@ -30,11 +21,10 @@ $mac=implode(':', str_split(substr(md5(mt_rand()), 0, 12), 2));
 $file=file("source.m3u");
 $i=0;
 foreach ($file as $f){
-	if (strpos($f,"DEVICEID")!==false) {
-		$uuid4=guidv4();
+	if (strpos($f,"DEVICEID")!==false) {		
 		$uuid1=gen_uuid_timebased($mac);
-		//echo "uuid1: $uuid1 \t uuid4: $uuid4\r\n";
-		$file[$i]=str_replace(array("DEVICEID","SID"),array($uuid1,$uuid4),$f);
+		//echo "uuid1: $uuid1 \r\n";
+		$file[$i]=str_replace("DEVICEID",$uuid1,$f);
 	}
 	echo $file[$i];
 	$i++;
